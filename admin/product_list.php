bcfc3c5f4edfc29d0abc95a436f76df4e22f8dcd
anastasiakotolupova <40686941+anastasiakotolupova@users.noreply.github.com>
2018-07-13 14:24:48 +0200

    admin folder

--- admin/product_list.php
+++ admin/product_list.php
@@ -2,8 +2,8 @@
 
     require_once('inc/header.php');
 
-    $result = $pdo->query("SELECT * FROM user");
-    $users = $result->fetchAll();
+    $result = $pdo->query("SELECT * FROM product");
+    $products = $result->fetchAll();
 
     $content .= "<table class='table table-striped'>";
     $content .= "<thead class='thead-dark'><tr>";
@@ -15,63 +15,63 @@
     }
     $content .= '<th colspan="2">Actions</th>';
     $content .= "</tr></thead><tbody>";
-    foreach ($users as $user) 
+    foreach ($products as $product) 
     {
         $content .= "<tr>";
-        foreach ($user as $key => $value) 
+        foreach ($product as $key => $value) 
         {
             if ($key == 'picture') 
             {
-                $content .= '<td><img height="100" src="' . URL . 'uploads/img/' . $value . '" alt="' . $user['user'] . '"/></td>';
+                $content .= '<td><img height="100" src="' . URL . 'uploads/img/' . $value . '" alt="' . $product['title'] . '"/></td>';
             } 
             else 
             {
                 $content .= "<td>" . $value . "</td>";
             }
         }
-        $content .= "<td><a href='" . URL . "admin/users_form.php?id=" . $user['user'] . "'><i class='fas fa-pen'></i></a></td>";
-        $content .= "<td><a href='?id=" . $user['user'] . "'><i class='fas fa-trash-alt'></i></a></td>";
+        $content .= "<td><a href='" . URL . "admin/product_form.php?id=" . $product['id_product'] . "'><i class='fas fa-pen'></i></a></td>";
+        $content .= "<td><a href='?id=" . $product['id_product'] . "'><i class='fas fa-trash-alt'></i></a></td>";
         $content .= "</tr>";
     }
     $content .= "</tbody></table>";
 
     if(isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id']))
     {
-        $req = "SELECT * FROM user WHERE user = :user";
+        $req = "SELECT * FROM product WHERE id_product = :id_product";
 
         $result = $pdo->prepare($req);
 
-        $result->bindValue(':user', $_GET['id'], PDO::PARAM_INT);
+        $result->bindValue(':id_product', $_GET['id'], PDO::PARAM_INT);
 
         $result->execute();
 
         if($result->rowCount() == 1)
         {
-            $user = $result->fetch();
+            $product = $result->fetch();
 
-            $delete_req = "DELETE FROM user WHERE user = $user[user]";
+            $delete_req = "DELETE FROM product WHERE id_product = $product[id_product]";
 
             $delete_result = $pdo->exec($delete_req);
 
             if($delete_result)
             {
-                $picture_path = ROOT_TREE . 'uploads/img/' . $user['picture'];
+                $picture_path = ROOT_TREE . 'uploads/img/' . $product['picture'];
 
-                if(file_exists($picture_path) && $user['picture'] != 'default.jpg') // function file_exists() allows us to be sure that we got this picture registered on the server
+                if(file_exists($picture_path) && $product['picture'] != 'default.jpg') // function file_exists() allows us to be sure that we got this picture registered on the server
                 {
                     unlink($picture_path); // function unlink() allows us to delete a file from the server
                 }
 
-                header('location:users_list.php?m=success');
+                header('location:product_list.php?m=success');
             }
             else
             {
-                header('location:users_list.php?m=fail');
+                header('location:product_list.php?m=fail');
             }
         }
         else
         {
-            header('location:users_list.php?m=fail');
+            header('location:product_list.php?m=fail');
         }
     }
 
@@ -96,7 +96,7 @@
 
 ?>
 
-<h1>List of users</h1>
+<h1>List of products</h1>
 
 
 <?= $msg_error ?>
