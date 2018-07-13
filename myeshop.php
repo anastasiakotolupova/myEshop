221fe7c5e6dc9535f8a3fe2d50b7c047cdd3dffe
preva-l <40019704+preva-l@users.noreply.github.com>
2018-07-13 12:07:04 +0200

    Front-end
    A bit ugly for the moment but usable

---
+++ myeshop.php
@@ -0,0 +1,56 @@
+<?php
+require_once('inc/header.php');
+
+ ?>
+
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">Buy everything you love.</h1>
+          <a href="#" class="btn btn-secondary my-2">All the product</a>
+            <a href="#" class="btn btn-info my-2">Woman</a>
+            <a href="#" class="btn btn-info my-2">Man</a>
+        </div>
+      </section>
+
+      <div class="album py-5 bg-light">
+        <div class="container">
+
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+<?php
+
+require_once('inc/footer.php');
+ ?>
---
+++ profile.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('inc/header.php');
+
+$page="Profile";
+ ?>
+
+ <h1> <?= $page ?></h1>
+
+
+ <?php
+
+require_once('inc/header.php');
+  ?>
