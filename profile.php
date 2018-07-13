a98c6845318b8e6b67844a9dc91cc29142ecd3eb
preva-l <40019704+preva-l@users.noreply.github.com>
2018-07-13 15:06:16 +0200

    New profile.php (with picture)

--- profile.php
+++ profile.php
@@ -2,13 +2,27 @@
 
 require_once('inc/header.php');
 
+if(!userConnect()){
+  header('location: login.php');
+  exit();
+}
+
 $page="Profile";
  ?>
 
  <h1> <?= $page ?></h1>
+ <p> Please fin the information below: </p>
+
+<ul>
+  <li><img src="uploads/img/<?= $_SESSION['user']['picture'] ?>" alt=""></li>
+  <li>firstname:  <?= $_SESSION['user']['firstname']?></li>
+  <li>lastname:  <?= $_SESSION['user']['lastname'] ?></li>
+  <li>email: <?= $_SESSION['user']['email'] ?></li>
+
+</ul>
 
 
  <?php
 
-require_once('inc/header.php');
+require_once('inc/footer.php');
   ?>
